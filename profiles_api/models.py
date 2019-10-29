835814fcaf3de848d91544f25c027f1b082f3df1
snooder <ankit230698@gmail.com>
2019-10-30 01:08:23 +0530

    django admin superuser

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -11,7 +11,7 @@ class UserProfileManager(BaseUserManager):
         if not email:
             raise ValueError('User must have an email add..')
         email = self.normalize_email(email)
-        user = self.Model(email=email, name=name)
+        user = self.model(email=email, name=name)
 
         user.set_password(password)
         user.save(using=self._db)
